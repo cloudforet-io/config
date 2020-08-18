39c94da1759f6e7a50504e57f158f6559964bb28
Jongmin Kim <whdalsrnt@megazone.com>
2020-08-18 21:57:01 +0900

    Add a local cache settings
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/spaceone/config/conf/global_conf.py
+++ src/spaceone/config/conf/global_conf.py
@@ -1,10 +1,19 @@
 DATABASES = {
     'default': {
-        'db': 'statistics',
+        'db': 'config',
         'host': 'localhost',
         'port': 27017,
         'username': '',
         'password': ''
+    }
+}
+
+CACHES = {
+    'default': {},
+    'local': {
+        'backend': 'spaceone.core.cache.local_cache.LocalCache',
+        'max_size': 128,
+        'ttl': 86400
     }
 }
 
