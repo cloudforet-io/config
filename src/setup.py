fd401f726b4bd4ebc248f622bf9bc0e8d3f01869
HanHyung Lee <167288002+lhhyung@users.noreply.github.com>
2025-11-14 14:18:30 +0900

    build: Rename package to 'spaceone_config' (PEP 625 compliance)
    Signed-off-by: HanHyung Lee <167288002+lhhyung@users.noreply.github.com>

--- src/setup.py
+++ src/setup.py
@@ -18,7 +18,7 @@ import os
 from setuptools import setup, find_packages
 
 setup(
-    name="spaceone-config",
+    name="spaceone_config",
     version=os.environ.get("PACKAGE_VERSION"),
     description="SpaceONE config service",
     long_description="",
